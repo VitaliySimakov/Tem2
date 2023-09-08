c61040cc4d154ac7852d44ffa1d45ad984688058
VitaliySimakov <143637261+VitaliySimakov@users.noreply.github.com>
2023-09-08 22:09:16 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+﻿//ЗАДАЧА 1
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 //a = 5; b = 7 -> max = 7
 //a = 2 b = 10 -> max = 10
 //a = -9 b = -3 -> max = -3
-
-
 //Console.WriteLine("введите первое число: ");
 //int number1 = int.Parse(Console.ReadLine());
 //Console.WriteLine("введите Второе число: ");
@@ -12,6 +11,22 @@
 //Console.WriteLine(number1);
 //else
 //    Console.WriteLine(number2);
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+//Console.WriteLine("введите первое число: ");
+//int number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите Второе число: ");
+//int number2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите Второе число: ");
+//int number3 = int.Parse(Console.ReadLine());
+//int max=number1;
+//if (number2 > max) max=number2;
+//if (number3 > max) max=number3;
+//Console.WriteLine(max);
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
 //является ли число чётным (делится ли оно на два без остатка).
