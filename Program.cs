e3fe0ba177817bd4b0b2c2a503c809556b3fbf05
VitaliySimakov <143637261+VitaliySimakov@users.noreply.github.com>
2023-09-08 22:02:46 +0400

    Add cod #6

--- Program.cs
+++ Program.cs
@@ -4,11 +4,41 @@
 //a = -9 b = -3 -> max = -3
 
 
-Console.WriteLine("введите первое число: ");
-int number1 = int.Parse(Console.ReadLine());
-Console.WriteLine("введите Второе число: ");
-int number2 = int.Parse(Console.ReadLine());
-if (number1 > number2)
-Console.WriteLine(number1);
-else
-    Console.WriteLine(number2);
+//Console.WriteLine("введите первое число: ");
+//int number1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("введите Второе число: ");
+//int number2 = int.Parse(Console.ReadLine());
+//if (number1 > number2)
+//Console.WriteLine(number1);
+//else
+//    Console.WriteLine(number2);
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+//Console.WriteLine("введите число: ");
+//int number1 = int.Parse(Console.ReadLine());
+//if (number1 %2 ==0) 
+//System.Console.WriteLine("Да");
+//    else 
+//    System.Console.WriteLine("Нет");
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+int num = int.Parse(Console.ReadLine());
+int i = 2;
+if (i>num) Console.WriteLine("Четных чисел от 1 до введенного числа нет");
+while (i<=num)
+{
+   if ((i%2==0 && i==num) || (i%2==0 && i+1==num))
+    {    
+        Console.Write(i);
+    }
+    else if (i % 2== 0)
+    {    
+        Console.Write(i+ ",");
+    }
+    i++;
+}
