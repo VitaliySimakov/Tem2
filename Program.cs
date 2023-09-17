fadd05c4b5ce894430d18c8ba88d7d810690f851
VitaliySimakov <143637261+VitaliySimakov@users.noreply.github.com>
2023-09-17 20:18:19 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,59 +1,60 @@
-﻿//ЗАДАЧА 1
-//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-//a = 5; b = 7 -> max = 7
-//a = 2 b = 10 -> max = 10
-//a = -9 b = -3 -> max = -3
-//Console.WriteLine("введите первое число: ");
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+
+//Console.Write("введите число: ");
 //int number1 = int.Parse(Console.ReadLine());
-//Console.WriteLine("введите Второе число: ");
+//Console.Write("введите степень числа: ");
 //int number2 = int.Parse(Console.ReadLine());
-//if (number1 > number2)
-//Console.WriteLine(number1);
-//else
-//    Console.WriteLine(number2);
+//double pow = Math.Pow(number1,number2);
+//Console.Write(pow);
 
-//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-//2, 3, 7 -> 7
-//44 5 78 -> 78
-//22 3 9 -> 22
+//Задача 27: Напишите программу, которая принимает 
+//на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
 
-//Console.WriteLine("введите первое число: ");
-//int number1 = int.Parse(Console.ReadLine());
-//Console.WriteLine("введите Второе число: ");
-//int number2 = int.Parse(Console.ReadLine());
-//Console.WriteLine("введите Второе число: ");
-//int number3 = int.Parse(Console.ReadLine());
-//int max=number1;
-//if (number2 > max) max=number2;
-//if (number3 > max) max=number3;
-//Console.WriteLine(max);
+//int WorkU(String msg)
+//{
+//    System.Console.WriteLine("Введите число: ");
+//    string mass = System.Console.ReadLine();
+//    int number=int.Parse(mass);
+//    return number;
+//}
+//int Summ(int num)
+//{
+//    int number = 0;
+//    while(num>0)
+//    {
+//        number += num %10;
+//        num = num/10;
+//    }
+//    return number;
+//}
+//int num = WorkU("Введите число: ");
+//System.Console.WriteLine($"Сумма всех чисел {num} = {Summ(num)}"
 
-//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
-//является ли число чётным (делится ли оно на два без остатка).
+//Задача 29: Напишите программу, которая задаёт массив 
+//из 8 элементов и выводит их на экран.
 
-//Console.WriteLine("введите число: ");
-//int number1 = int.Parse(Console.ReadLine());
-//if (number1 %2 ==0) 
-//System.Console.WriteLine("Да");
-//    else 
-//    System.Console.WriteLine("Нет");
+//int[] numbers = new int[8];
+//FillArray(numbers);
+//PrintArray(numbers);
+//void FillArray (int[] array)
+//{
+//    for(int i=0;i<array.Length; i++)
+//    {
+//        array[i]=new Random().Next(0,9);
+//    }
+//}
+//void PrintArray(int[] Collection)
+//{
+//    for (int i=0;i<Collection.Length;i++)
+//    {
+//        Console.Write(Collection[i]+ " ");
+//    }
+//}
 
-//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-//5 -> 2, 4
-//8 -> 2, 4, 6, 8
 
-int num = int.Parse(Console.ReadLine());
-int i = 2;
-if (i>num) Console.WriteLine("Четных чисел от 1 до введенного числа нет");
-while (i<=num)
-{
-   if ((i%2==0 && i==num) || (i%2==0 && i+1==num))
-    {    
-        Console.Write(i);
-    }
-    else if (i % 2== 0)
-    {    
-        Console.Write(i+ ",");
-    }
-    i++;
-}
