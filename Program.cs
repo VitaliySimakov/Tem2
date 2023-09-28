e1127af98d22b4835114c0e52705a54509771f3e
VitaliySimakov <143637261+VitaliySimakov@users.noreply.github.com>
2023-09-28 18:36:00 +0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,60 +1,124 @@
-//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
-//и возводит число A в натуральную степень B.
-//3, 5 -> 243 (3⁵)
-//2, 4 -> 16
+//Задача 34: Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет 
+//количество чётных чисел в массиве.
+/*
+int size = WorkWithUser("Введите размер массива: ");
+int minValue=WorkWithUser("Введите минимальное значение диапазона: ");
+int maxValue=WorkWithUser("Введите максимальное значение диапазона: ");
+int[] array=GetArray(size,minValue,maxValue);
+PrintArray(array);
 
-//Console.Write("введите число: ");
-//int number1 = int.Parse(Console.ReadLine());
-//Console.Write("введите степень числа: ");
-//int number2 = int.Parse(Console.ReadLine());
-//double pow = Math.Pow(number1,number2);
-//Console.Write(pow);
+int WorkWithUser(String message)
+{
+    Console.Write(message);
+    int number=int.Parse(Console.ReadLine());
+    return number;
+}
+int[] GetArray(int Length, int min, int max)
+{
+    int[] result=new int[Length];
+    Random rand=new Random();
+    for(int i=0; i<result.Length;i++)
+    {
+        result[i]=rand.Next(min,max+1);
+    }
+    return result;
+}
+void PrintArray(int[]arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        Console.Write(arr[i]+ " ");
+    }
+}
+int kon=0;    
+for(int i=0; i<array.Length;i++)
+    {
+        if(array[i]%2==0)
+        {
+            kon++;
+        }
+    }
+Console.Write(" ");
+Console.Write($"{kon}");
 
-//Задача 27: Напишите программу, которая принимает 
-//на вход число и выдаёт сумму цифр в числе.
-//452 -> 11
-//82 -> 10
-//9012 -> 12
+Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
 
-//int WorkU(String msg)
-//{
-//    System.Console.WriteLine("Введите число: ");
-//    string mass = System.Console.ReadLine();
-//    int number=int.Parse(mass);
-//    return number;
-//}
-//int Summ(int num)
-//{
-//    int number = 0;
-//    while(num>0)
-//    {
-//        number += num %10;
-//        num = num/10;
-//    }
-//    return number;
-//}
-//int num = WorkU("Введите число: ");
-//System.Console.WriteLine($"Сумма всех чисел {num} = {Summ(num)}"
+int size = WorkWithUser("Введите размер массива: ");
+int minValue=WorkWithUser("Введите минимальное значение диапазона: ");
+int maxValue=WorkWithUser("Введите максимальное значение диапазона: ");
+int[] array=GetArray(size,minValue,maxValue);
+PrintArray(array);
 
-//Задача 29: Напишите программу, которая задаёт массив 
-//из 8 элементов и выводит их на экран.
+int WorkWithUser(String message)
+{
+    Console.Write(message);
+    int number=int.Parse(Console.ReadLine());
+    return number;
+}
+int[] GetArray(int Length, int min, int max)
+{
+    int[] result=new int[Length];
+    Random rand=new Random();
+    for(int i=0; i<result.Length;i++)
+    {
+        result[i]=rand.Next(min,max+1);
+    }
+    return result;
+}
+void PrintArray(int[]arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        Console.Write(arr[i]+ " ");
+    }
+}
+int kon=0;    
+for(int i=0; i<array.Length;i++)
+    {
+        if(array[i]%2==1)
+        {
+            kon=kon+array[i];
+        }
+    }
+Console.Write(" ");
+Console.Write($"{kon}");
 
-//int[] numbers = new int[8];
-//FillArray(numbers);
-//PrintArray(numbers);
-//void FillArray (int[] array)
-//{
-//    for(int i=0;i<array.Length; i++)
-//    {
-//        array[i]=new Random().Next(0,9);
-//    }
-//}
-//void PrintArray(int[] Collection)
-//{
-//    for (int i=0;i<Collection.Length;i++)
-//    {
-//        Console.Write(Collection[i]+ " ");
-//    }
-//}
+//Задача 38: Задайте массив вещественных чисел. Найдите разницу между 
+//максимальным и минимальным элементов массива.
+//[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
+Console.Write("Введите количество элементов массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] randomArray = new double[a];
+
+void mas(int a)
+{
+    Random rand = new Random();
+    for (int i = 0; i < a; i++)
+    {
+    randomArray[i] = rand.NextDouble();
+    Console.Write($"{randomArray[i]:F2} ");
+    }
+
+}
+double raz(double[] randomArray)
+{
+double min = randomArray[0];
+double max = randomArray[0];
+int i = 1;
+while (i < randomArray.Length)
+{
+if (max<randomArray[i])
+max = randomArray[i];
+if (min>randomArray[i])
+min = randomArray[i];
+i = i + 1;
+}
+return max-min;
+}
+
+mas(a);
+Console.Write($"\nРазница между максимальным и минимальным элементов массива: {raz(randomArray):F2}");
 
